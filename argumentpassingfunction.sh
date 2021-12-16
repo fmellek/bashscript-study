4a952d51c5478eaddb3ecc8e99b9f828e7960c58
fmellek <ellek528@gmail.com>
2021-12-16 00:55:00 +0000

    shortcuts for commands

---
+++ argumentpassingfunction.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+function hello(){
+    echo "Hello $1"
+}
+hello FME
+
+#######################################
+   # Description: Hello function
+   # Globals:
+   #   None
+# Arguments:
+# Single input argument
+# Outputs:
+# Value of input argument
+# Returns:
+# 0 if successful, non-zero on error. #######################################
