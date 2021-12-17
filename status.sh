5cae341d3956d0a1783685256309a99635534b0b
fmellek <ellek528@gmail.com>
2021-12-17 07:20:23 +0000

    disk usage,cpu usage,ram usage has been checked on server with bashscript functions.

--- status.sh
+++ status.sh
@@ -12,3 +12,42 @@ function memory_check() {
 }
 #call the function
 memory_check
+
+function disk_usage(){
+    echo ""
+      echo "The current disk usage on ${server_name} is: "
+      df -h
+      echo "" 
+}
+#call the function
+disk_usage
+
+function cpu_usage(){
+    echo ""
+      echo "The current cpu usage on ${server_name} is: "
+      uptime
+      echo ""
+}
+
+#call the function
+cpu_usage
+
+function kernel_check(){
+  echo ""
+    echo "Kernel version on ${server_name} is: "
+    echo ""
+    uname -r
+  echo ""
+}
+
+#call the function
+kernel_check
+
+function all_checks(){
+  memory_check
+  disk_usage
+  cpu_usage
+  kernel_check
+}
+#call the function
+all_checks
