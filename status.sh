56022de472a17e0eed395efa4b79fbb5f43af526
fmellek <ellek528@gmail.com>
2021-12-20 03:08:32 +0000

    menu has been studied on bash

--- status.sh
+++ status.sh
@@ -51,3 +51,42 @@ function all_checks(){
 }
 #call the function
 all_checks
+
+#Color Variables
+green='\e[32m'
+blue='\e[34m'
+clear='\e[0m'
+
+#Color Functions
+ColorGreen(){
+    echo -ne $green$1$clear
+}
+ColorBlue(){
+    echo -ne $blue$1$clear
+}
+echo -ne $(ColorBlue 'Some text here') #This will output the Some text here string and it woulb be blue!
+ColorGreen
+ColorBlue
+
+menu(){
+  echo -ne "
+  My First Menu
+  $(ColorGreen '1)') Memory usage
+  $(ColorGreen '2)') CPU load
+  $(ColorGreen '3)') Number of TCP connections
+  $(ColorGreen '4)') Kernel Version
+  $(ColorGreen '5)') Check All
+  $(ColorGreen '0)') Exit
+  $(ColorBlue 'Choose an option:') "
+    read a
+    case $a in
+      1) memory_check ; menu ;;
+      2) cpu_usage ; menu ;;
+      3) disk_usage ; menu ;;
+      4) kernel_check ; menu ;;
+      5) all_checks ; menu ;;
+        0) exit 0 ;;
+        *) echo -e $red"Wrong option."$clear;
+  WrongCommand;;
+    esac
+}
